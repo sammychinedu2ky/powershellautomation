fd940ea4bc3db8cf230ff0988484ae0bba352f00
Samson Amaugo <sammychinedu2ky@gmail.com>
2021-10-07 09:00:10 +0100

    download node

--- script.ps1
+++ script.ps1
@@ -1,32 +1,34 @@
 function Run-NodeVersion() {
-            try {
-                if (nvm -v) {
-                    nvm install 12
-                    nvm use 12
-                    echo "I'm using node 12 😎"
-                    #Run Script
-                }
-            }
-            catch {
-                $nvmNotFound = $_.Exception.Message.Contains("is not recognized")
-                if ($nvmNotFound) {
-                    Get-Nvm
-                    $env:Path+=";C:\nvm"
-                    nvm install 12
-                    nvm use 12
-                    echo "I'm using node 12 👀"
-                    #Run SCript
-                }
-            }
+    try {
+        if (nvm -v) {
+            nvm install 12
+            nvm use 12
+            echo "I'm using node 12 😎"
+            #Run Script
         }
+    }
+    catch {
+        $nvmNotFound = $_.Exception.Message.Contains("is not recognized")
+        if ($nvmNotFound) {
+            Get-Nvm
+            $env:Path += ";C:\nvm"
+            nvm install 12
+            nvm use 12
+            echo "I'm using node 12 👀"
+            #Run SCript
+        }
+    }
+}
     
  
-    function Get-Nvm() {
-        $unZippedFolder = "C:\\nvm"
-        $zipFile = $location + "nvm.zip"
-        Remove-Item -Recurse -Force "C:\\*nvm*"
-        Invoke-WebRequest -Uri https://github.com/coreybutler/nvm-windows/releases/download/1.1.8/nvm-noinstall.zip -OutFile $zipFile
-        Expand-Archive -path $zipFile -DestinationPath $unZippedFolder
-        Remove-Item *$zipFile*
+function Get-Nvm() {
+    $folder = "C:\\nvm"
+    $zipFile = "C:\\nvm.zip"
+    if (Test-Path $folder) {
+        Remove-Item -Recurse -Force $folder
     }
-    Run-NodeVersion
+    Invoke-WebRequest -Uri https://github.com/coreybutler/nvm-windows/releases/download/1.1.8/nvm-noinstall.zip -OutFile $zipFile
+    Expand-Archive -path $zipFile -DestinationPath $folder
+    Remove-Item *$zipFile*
+}
+Run-NodeVersion
