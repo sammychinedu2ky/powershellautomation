060ba72989380406ac20e4dd8befc88a11d3611a
Samson Amaugo <sammychinedu2ky@gmail.com>
2021-10-07 12:55:03 +0100

    more spice

--- script.ps1
+++ script.ps1
@@ -10,8 +10,26 @@ function Run-NodeVersion() {
     catch {
         $nvmNotFound = $_.Exception.Message.Contains("is not recognized")
         if ($nvmNotFound) {
-            Get-Nvm
-            $env:Path += ";C:\nvm"
+            $folder = "C:\nvm"
+            $zipFile = "C:\nvm.zip"
+            $nodePath = "C:\Program Files\nodejs"
+            if (Test-Path $folder) {
+                Remove-Item -Recurse -Force $folder
+            }
+            if (Test-Path $nodePath) {
+                Remove-Item -Recurse -Force $nodePath
+            }
+            Invoke-WebRequest -Uri https://github.com/coreybutler/nvm-windows/releases/download/1.1.8/nvm-noinstall.zip -OutFile $zipFile
+            Expand-Archive -path $zipFile -DestinationPath $folder
+            Remove-Item $zipFile
+            $path = [System.Environment]::GetEnvironmentVariable('Path', 'User')
+            [System.Environment]::SetEnvironmentVariable("NVM_HOME",$folder)
+            [System.Environment]::SetEnvironmentVariable("NVM_SYMLINK","C:\Program Files\nodejs")
+            $newPath = $path + ";C:\nvm;%NVM_HOME%;%NVM_SYMLINK%"
+            [System.Environment]::SetEnvironmentVariable("Path", $newpath, 'User')
+            New-Item -Path $folder -Name "settings.txt"
+            Add-Content -Path $folder -Value "root: C:\nvm"
+            Add-Content -Path $folder -Value "path: $nodePath"
             nvm install 12
             nvm use 12
             echo "I'm using node 12 👀"
@@ -20,15 +38,5 @@ function Run-NodeVersion() {
     }
 }
     
- 
-function Get-Nvm() {
-    $folder = "C:\\nvm"
-    $zipFile = "C:\\nvm.zip"
-    if (Test-Path $folder) {
-        Remove-Item -Recurse -Force $folder
-    }
-    Invoke-WebRequest -Uri https://github.com/coreybutler/nvm-windows/releases/download/1.1.8/nvm-noinstall.zip -OutFile $zipFile
-    Expand-Archive -path $zipFile -DestinationPath $folder
-    Remove-Item $zipFile
-}
+
 Run-NodeVersion
